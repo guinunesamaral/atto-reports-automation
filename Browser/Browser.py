05557bcb690c152f23893de4c6d81e8cb30e8e51
Guilherme Nunes Amaral dos Santos <guilherme.santos@vikservices.com.br>
2023-09-15 16:57:15 -0300

    Mudança para remover biblioteca com problema

--- Browser/Browser.py
+++ Browser/Browser.py
@@ -1,29 +1,22 @@
+import os
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from FileManager.FileManager import FileManager
-import chromedriver_autoinstaller
 
 
 class Browser:
     def __init__(self):
         pass
 
-    # Check if the current version of chromedriver exists
-    # and if it doesn't exist, download it automatically,
-    # then add chromedriver to path
-    def install_chromedriver(self):
-        chromedriver_autoinstaller.install()
-
     def get_chrome(self):
-        self.install_chromedriver()
-
         file_manager = FileManager()
         prefs = {
             'download.default_directory': file_manager.concatenar_root_com_path('downloads')}
 
         options = ChromeOptions()
+        options.binary_location = os.environ.get("CHROME_FOR_TESTING_PATH")
         options.add_experimental_option("prefs", prefs)
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        options.add_argument("--headless=new")
+        # options.add_argument("--headless=new")
 
         return webdriver.Chrome(options=options)
